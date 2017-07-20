5a7f0f563c2bdb19c2883aaddc2ba90c8c68e0ea
Selvamani <sselvamani22@gmail.com>
2017-07-20 10:55:25 +0530

    Added info for page

--- src/components/main/WeatherList.js
+++ src/components/main/WeatherList.js
@@ -5,7 +5,18 @@ function WeatherList({list}) {
 
   return (
     <li>
-      {list.dt_txt}
+      Time {list.dt_txt}
+      <img src={'http://openweathermap.org/img/w/' + list.weather[0].icon + '.png'} />
+      <b>
+        {list.weather.description}
+      </b>
+      <p>
+        Cloud
+        <span class="badge badge-info">
+          {list.clouds.all}%
+        </span>
+        Temperature {list.main.temp_min} to {list.main.temp_max}
+      </p>
     </li>
   );
 }
